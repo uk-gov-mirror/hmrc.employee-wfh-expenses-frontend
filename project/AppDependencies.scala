c8551c452a17efda79cd6d23f6ac8ff008be81f8
grahampaulcook <grahampaulcook@users.noreply.github.com>
2021-03-17 17:31:04 +0000

    latest deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,8 +8,8 @@ object AppDependencies {
     "uk.gov.hmrc"         %% "logback-json-logger"            % "5.1.0",
     "uk.gov.hmrc"         %% "play-health"                    % "3.16.0-play-27",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping"  % "1.6.0-play-27",
-    "uk.gov.hmrc"         %% "bootstrap-frontend-play-27"     % "3.4.0",
-    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "0.50.0-play-27",
+    "uk.gov.hmrc"         %% "bootstrap-frontend-play-27"     % "4.1.0",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "0.51.0-play-27",
     "uk.gov.hmrc"         %% "play-partials"                  % "7.1.0-play-27",
     "uk.gov.hmrc"         %% "tax-year"                       % "1.2.0",
     "com.digitaltangible" %% "play-guard"                     % "2.4.0"
