37c5a36adb90c9ddd9993e7b0f57a2c263f74757
Mathew Joseph <183598333+matjoseph24@users.noreply.github.com>
2026-06-23 12:23:52 +0100

    DL-19578: Bump sca-wrapper to 5.3.0 (#231)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,21 +2,21 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.7.0"
-  val mongoPlayVersion = "2.12.0"
+  val scaWrapperPlayVersion = "5.3.0"
+  val mongoPlayVersion      = "2.12.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoPlayVersion,
     "uk.gov.hmrc"       %% "tax-year"                              % "6.0.0",
-    "uk.gov.hmrc"       %% "sca-wrapper-play-30"                   % "5.2.0"
+    "uk.gov.hmrc"       %% "sca-wrapper-play-30"                   % scaWrapperPlayVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoPlayVersion,
-    "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0"
+    "uk.gov.hmrc"       %% "sca-wrapper-test-play-30" % scaWrapperPlayVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30"  % mongoPlayVersion,
+    "org.scalatestplus" %% "scalacheck-1-17"          % "3.2.18.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
